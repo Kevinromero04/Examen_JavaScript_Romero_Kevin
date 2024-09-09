1d17522a1743c054f0903de3f2ada88056f9a568
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-09-09 12:59:25 -0500

    feat: :sparkles: creacion de modulos y readme
    Se completa casi al 100% el proyecto y se completo el readme en la pagina falto unos filtros
    
    By @Kevinromero04

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# Examen_JavaScript_Romero_Kevin
+
+# Chismeslands
+
+## Descripción
+
+"chismeslands" es una una pagina donde puedes contar todos los chismes sobres los campers :).
+
+## Tecnologías Utilizadas
+
+- **HTML**: Estructura y contenido de las páginas web.
+- **CSS**: Estilos y diseño visual de la aplicación.
+- **JavaScript**: Lógica y funcionalidad interactiva de la aplicación.
+
+## Estructura del Proyecto
+
+1. **Inicio de Sesión (index.html)**
+   - Aqui nos puedes contar todos tus chismes.
+
+
+## Funcionalidades
+
+### 1. Registro de chisme
+- Permite registrar el chisme en el sistema.
+- Cada chisme se le coloca un identificador.
+- Los chismes tienen categorias: Personal, laboral y familiar.
+- Tambine se pueden cguardar como Contados o guardados
+
+
+
+### 3. Filtros dew chismes
+- Se pueden mostrar todos los chismes o solo los contados y guardados
+
+## Instalación
+
+1. Clona el repositorio:
+
+   ```bash
+   git clone <https://github.com/Kevinromero04/Examen_JavaScript_Romero_Kevin.git>
+   ```
+
+2. Navega al directorio del proyecto:
+
+   ```bash
+   cd <Examen_JavaScript_Romero_Kevin
+>
+   ```
+
+3. Abre `index.html` en tu navegador para ver la pagina en funcionamiento.
+
+
+## Contribuciones
+
+Las contribuciones son bienvenidas. Para contribuir:
+
+1. Realiza un fork del repositorio.
+2. Crea una nueva rama (`git checkout -b feature-nueva`).
+3. Realiza tus cambios y haz commits (`git commit -am 'Añadir nueva funcionalidad'`).
+4. Envía un pull request (`git push origin feature-nueva`).
+
+## Contacto
+
+Para más información, contacta a [Micorreo](kevinromerorincon7@gmail.com).
+
+---
+
---
+++ chismes.js
@@ -0,0 +1,193 @@
+const chisme = document.querySelector(".envia_chisme")
+chisme.addEventListener("click", guardar_chisme)
+
+
+
+
+async function guardar_chisme(){
+    const el_chisme = document.querySelector("#descripcion").value
+    const tipo = document.querySelector("#categoria").value
+    const estado = document.querySelector("#estado").value
+    const coment = document.querySelector("#comentarios").value
+    const hoy = document.querySelector("#fecha").value
+    const identifica = document.querySelector("#identificador").value
+    var lista2 = JSON.parse(localStorage.getItem("guardado")) || []
+    var lista3 = JSON.parse(localStorage.getItem("contado")) || []
+    var correccion = false
+    lista2.forEach(element => {
+        if(element.descripcion === el_chisme){
+            alert("Este chisme ya fue contado")
+            correccion = true
+            
+        }
+        if(element.id === identifica){
+            alert("Este cidentificador ya fue usado porfavor usa otro")
+            correccion = true
+        }
+    })
+
+    lista3.forEach(element => {
+        if(element.descripcion === el_chisme){
+            alert("Este chisme ya fue contado")
+            correccion = true
+            
+        }
+        if(element.id === identifica){
+            alert("Este cidentificador ya fue usado porfavor usa otro")
+            correccion = true
+        }
+    })
+
+    if (!el_chisme || !tipo || !estado || !hoy) {
+        alert("porfavor llena todos los campos")
+        return
+    }
+
+    if(correccion === true){
+        return
+    }
+
+    if( estado === "Guardado" ){
+        let chisme_guardado = {
+            categoria: tipo,
+            descripcion: el_chisme,
+            comentario: coment,
+            fecha: hoy,
+            id: identifica
+        }
+        
+        lista2.push(chisme_guardado)
+        localStorage.setItem("guardado", JSON.stringify(lista2))
+    }else if( estado === "Contado"){
+        let chisme_contado = {
+            categoria: tipo,
+            descripcion: el_chisme,
+            comentario: coment,
+            fecha: hoy,
+            id: identifica
+        }
+        
+        lista3.push(chisme_contado)
+        localStorage.setItem("contado", JSON.stringify(lista3))
+    }
+
+}
+
+const mostrador = document.querySelector(".todos")
+mostrador.addEventListener("click", mostrar_todos)
+
+function mostrar_todos(){
+    const mostrador = JSON.parse(localStorage.getItem("guardado"))
+    const mostrador2 = JSON.parse(localStorage.getItem("contado"))
+    const escribir = document.querySelector("#listaChismes")
+    escribir.innerHTML =""
+    mostrador.forEach(element => {
+        console.log(element)
+        
+        
+        escribir.innerHTML += `
+    <div class="divide">
+                    <p>Categoria: Guardados</p>
+                    <p>descripcion:${element.descripcion}</p>
+                    <p>categoria: ${element.categoria}</p>
+                    <p>fecha: ${element.fecha}</p>
+                    <p>comentario: ${element.comentario}</p>
+                    <p>Identificador: ${element.id}</p>
+                        
+                </div>
+    
+    `
+    });
+
+    mostrador2.forEach(element => {
+        console.log(element)
+        escribir.innerHTML += `
+    <div class="divide">
+                    <p>Categoria: Contados</p>
+                    <p>descripcion:${element.descripcion}</p>
+                    <p>categoria: ${element.categoria}</p>
+                    <p>fecha: ${element.fecha}</p>
+                    <p>comentario: ${element.comentario}</p>
+                    <p>Identificador: ${element.id}</p>
+                        
+                </div>
+    
+    `
+    });
+
+
+    
+
+}
+
+
+const mostrador2 = document.querySelector(".chis_guardado")
+mostrador2.addEventListener("click", mostrar_guardados)
+
+function mostrar_guardados(){
+    const mostrador = JSON.parse(localStorage.getItem("guardado"))
+    const escribir = document.querySelector("#listaChismes")
+    escribir.innerHTML =""
+    mostrador.forEach(element => {
+        
+        
+        escribir.innerHTML += `
+    <div class="divide">
+                    <p>Categoria: Guardados</p>
+                    <p>descripcion:${element.descripcion}</p>
+                    <p>categoria: ${element.categoria}</p>
+                    <p>fecha: ${element.fecha}</p>
+                    <p>comentario: ${element.comentario}</p>
+                    <p>Identificador: ${element.id}</p>
+                </div>
+    
+    `
+    });
+
+
+}
+
+
+
+const mostrador3 = document.querySelector(".chis_contado")
+mostrador3.addEventListener("click", mostrar_contado)
+
+function mostrar_contado(){
+    const mostrador2 = JSON.parse(localStorage.getItem("contado"))
+    const escribir = document.querySelector("#listaChismes")
+    escribir.innerHTML =""
+    
+mostrador2.forEach(element => {
+    escribir.innerHTML += `
+            <div class="divide">
+                <p>Categoria: Contados</p>
+                <p>descripcion:${element.descripcion}</p>
+                <p>categoria: ${element.categoria}</p>
+                <p>fecha: ${element.fecha}</p>
+                <p>comentario: ${element.comentario}</p>
+                <p>Identificador: ${element.id}</p>
+                    
+            </div>
+
+`
+});
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
